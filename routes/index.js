dc4d408d32df4066be9d797455348a10c37a766a
jianghurong <1164310634@qq.com>
2018-11-06 17:58:38 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 const db = require("../scripts/db");
 
 module.exports =  (router) => {
+  //upload file
+  router.post('/upload', async function (ctx, next) {
+    // ctx.response.set("Access-Control-Allow-Origin", '*');
+    ctx.body = ctx.request.body;
+    console.log(ctx.request.body);
+  })
+
   // retrieve user
   router.get('/retrieve/user', async function (ctx, next) {
     let list = await db.retrieveUsers();
@@ -21,6 +28,5 @@ module.exports =  (router) => {
     let description = query.description || "i would like request a sing";
     let res = await db.insertUser(name, icon, age, content, description);
     ctx.body = res;
-    return res;
   })
 }
