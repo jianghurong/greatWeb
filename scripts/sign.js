f75a761e6011813140c8523055cf01de5b0556b5
jianghurong <1164310634@qq.com>
2018-11-08 17:35:34 +0800

    update

---
+++ scripts/sign.js
@@ -0,0 +1,13 @@
+// getReqsign 根据 接口请求参数 和 应用秘钥 计算请求签名
+
+function getReqsign(params, appkey) {
+    let params = {
+        'aaa' : 'aaa',
+        'abc' : 'abc',
+        'abb' : 'abb',
+        '1aa' : '1aa',
+        'abd' : 'abd'
+    };
+    let appkey = 'jyylqTaZRfLa1zV3';
+    ksort();
+}
